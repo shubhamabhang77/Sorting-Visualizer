274474015e48e2e9acf3f906b8d241a6685244bb
Shubham Abhang <shubhamabhang77@gmail.com>
2020-05-08 01:04:39 +0530

    added minor changes

---
+++ build/precache-manifest.cda4849c5a97e8778100f3dd226bcba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60da72bad71db338f8620349a7613f6",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "0f3fd3d64fdd4ca67ffb",
+    "url": "/Sorting-Visualizer/static/css/main.125b764f.chunk.css"
+  },
+  {
+    "revision": "b04c1ab2eefa355674f7",
+    "url": "/Sorting-Visualizer/static/js/2.4d726813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Visualizer/static/js/2.4d726813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f3fd3d64fdd4ca67ffb",
+    "url": "/Sorting-Visualizer/static/js/main.8c80460b.chunk.js"
+  },
+  {
+    "revision": "84f584066171505b5f2e",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.2161ca51.js"
+  }
+]);
